fe0f2c02f9836f541ee32e72fbe699da37eec8a6
Ryan Lambe <ryanlambe@hotmail.com>
2024-12-09 19:14:47 -0500

    hello world

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    if (!glfwInit()) {
+        // todo: error
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "Particles", nullptr, nullptr);
+    if(!window) {
+        // todo: error
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        // todo
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
