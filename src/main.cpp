178b9691592e36275ac04f4eb40642c95695c997
Ryan Lambe <ryanlambe@hotmail.com>
2024-12-09 21:02:57 -0500

    added glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
 
+#define GLAD_GL_IMPLEMENTATION
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+namespace math = glm;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
     if (!glfwInit()) {
-        // todo: error
+        // todo: throw error
+        return -1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(1920, 1080, "Particles", nullptr, nullptr);
     if(!window) {
-        // todo: error
+        glfwTerminate();
+        // todo: throw error
+        return -1;
     }
 
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        // todo: throw error
+        return -1;
+    }
+
+    // start
+    std::cout << math::dot(math::vec3(1, 1, 0), math::vec3(1, 8, 0)) << "\n";
+
     while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // update
+        glClearColor(1, 0, 1, 1);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
